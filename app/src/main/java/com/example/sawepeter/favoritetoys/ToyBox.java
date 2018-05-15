b3c1e5e98153c0deaf01d652346763cef7b10546
user.email <sawepeter6@gmail.com>
2018-05-15 20:01:12 +0300

    getToyNames

--- app/src/main/java/com/example/sawepeter/favoritetoys/ToyBox.java
+++ app/src/main/java/com/example/sawepeter/favoritetoys/ToyBox.java
@@ -4,5 +4,10 @@ package com.example.sawepeter.favoritetoys;
  * Created by SAWE PETER on 5/15/2018.
  */
 
-public class ToyBox {
+public final class ToyBox {
+    public static String[] getToyNames(){
+        return new String[]{
+                
+        }
+    }
 }
