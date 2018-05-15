4c6f113cd9332e4b02d9ec6749548530baca7aa3
user.email <sawepeter6@gmail.com>
2018-05-15 20:12:37 +0300

    get resource by id

--- app/src/main/java/com/example/sawepeter/favoritetoys/MainActivity.java
+++ app/src/main/java/com/example/sawepeter/favoritetoys/MainActivity.java
@@ -2,12 +2,16 @@ package com.example.sawepeter.favoritetoys;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    TextView mToysListTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mToysListTextView = (TextView)findViewById(R.id.tv_toy_names);
     }
 }
