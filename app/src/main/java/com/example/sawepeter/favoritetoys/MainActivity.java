46c7ab10208a9dee7c5473dfc9a9cefac9fee3ad
user.email <sawepeter6@gmail.com>
2018-05-24 13:08:05 +0300

    added all untracked files 🍕

--- app/src/main/java/com/example/sawepeter/favoritetoys/MainActivity.java
+++ app/src/main/java/com/example/sawepeter/favoritetoys/MainActivity.java
@@ -6,19 +6,14 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView mToysListTextView;
+    
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mToysListTextView = (TextView)findViewById(R.id.tv_toy_names);
 
-        String[] toyNames = ToyBox.getToyNames();
-
-        for (String toyName : toyNames){
-            mToysListTextView.append(toyName + "\n\n\n");
         }
     }
 }
